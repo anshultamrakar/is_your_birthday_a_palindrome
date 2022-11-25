da2153b7ad37a8ced5a05c427c8e656d0f2dbfcb
Anshul Tamrakar <anshultamrakar487@gmail.com>
2022-11-25 23:52:42 +0530

    added functions

--- app.js
+++ app.js
@@ -17,11 +17,6 @@ function checkIsPalindrome(str){
    return str === newReverseStr
 }
 
-var date = {
-    day : 5,
-    month : 12,
-    year : 2020
-}
 
 
 function convertDateToStr(dateStr){
@@ -67,8 +62,85 @@ function checkPalindromeForAllDateFormats(date){
 }
 
 
+function isLeapYear(year){
+  if(year % 400 === 0 ){
+    return true;
+  }if(year % 100 === 0){
+    return false;
+  }
+  if(year % 4=== 0){
+    return true;
+  }
+
+}
 
 
+console.log(isLeapYear(2021))
+
+function getNextDate(date){
+     var day = date.day + 1 
+     var month = date.month 
+     var year = date.year
+
+     var dayInMonths = [31,28,31,30,31,30,31, 31,30,31,30 ,31]
+
+     if(dayInMonths === 2){
+        if(isLeapYear(year)){
+           if(day > 29){
+             day = 1 ;
+             month++
+           }else{
+            if(day > 29){
+                day = 1 
+                month++
+            }
+           }
+        }
+     }
+     else{
+        if(day > dayInMonths[month - 1]){
+            day = 1 ;
+            month++
+        }
+     }
+     if(month > 12){
+        month =1 ;
+        year++;
+     }
+     return{
+        day : day,
+        month : month,
+        year : year
+     };
+}
+
+
+
+function getNextPalindromeDate(date){
+  var ctr = 0 ;
+  var nextDate = getNextDate(date)
+  while(1){
+    ctr++;
+    var checkIsPalindrome = checkPalindromeForAllDateFormats(nextDate)
+    if(checkIsPalindrome){
+        break;
+    }
+    nextDate = getNextDate(nextDate)
+  }
+  return [ctr , nextDate]
+}
+
+
+var date = {
+    day : 11,
+    month : 2,
+    year : 2020
+}
+
+
+
+// console.log(getNextDate(date))
+console.log(getNextPalindromeDate(date))
 
 
 
